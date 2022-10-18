802908127260ed601ca0a18898e173e349b36ca1
Gabriel Sitorus <sitorusgg@gmail.com>
2022-10-18 11:12:42 +0700

    signup validations on user backend enpoint done

--- server/routes/api/session.js
+++ server/routes/api/session.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
-
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
-// Log in
+// Validate Login 
+// Checks to see whether or not req.body.credential and req.body.password are empty.
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+    handleValidationErrors
+]
+
 
 // Log in
 router.post(
     '/',
+    validateLogin,
     async (req, res, next) => {
         const { credential, password } = req.body;
         const user = await User.login({ credential, password });
@@ -52,5 +66,5 @@ router.get(
     }
 )
 
-// start on phase 5 on backend
+
 module.exports = router;
--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -2,13 +2,36 @@ const express = require('express');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const router = express.Router();
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
+
+// Validation Sign Up || Backend validations should be in an array
+const validateSignup = [
+    check('email')
+        .exists({ checkFalsy: true })
+        .isEmail()
+        .withMessage('Please enter a valid email.'),
+    check('username')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 4, max: 30 })
+        .withMessage('Please enter a username that is between 4 and 30 characters.'),
+    check('username)')
+        .not()
+        .isEmail()
+        .withMessage('The username cannot be an email.'),
+    check('password')
+        .exists({ checkFalse: true })
+        .isLength({ min: 4, max: 100 })
+        .withMessage('Please enter a password that is between 4 and 100 characters.'),
+    handleValidationErrors
+]
 // Sign up
 router.post(
     '/',
+    validateSignup,
     async (req, res) => {
         const { email, password, username } = req.body;
-        console.log(req.body, '@@@@@@@@@@@@@@@@@@@@@@')
         const user = await User.signup({
             email,
             password,
