48f78c64927b2e955d3e87b4401aa4c3dce18504
Gabriel Sitorus <sitorusgg@gmail.com>
2022-10-19 14:38:31 +0700

    created splah page

--- client/src/features/SplashPage/SplashPage.tsx
+++ client/src/features/SplashPage/SplashPage.tsx
@@ -10,18 +10,24 @@ function SplashPage({}: Props) {
 
   useEffect(() => {
     setTimeout(() => {
-      setIndex(index + 1)
-    }, 5000)
+      setIndex(index + 1);
+    }, 5000);
 
-    if (index === images.length - 1) setIndex(0)
-  }, [index])
+    if (index === images.length - 1) setIndex(0);
+  }, [index]);
   return (
     <div className="splashPage">
       <img src={images[index].src} alt="" className="splashPage__image" />
       <Header />
 
       <div className="splashPage__start">
-        
+        <h1 className="splashPage__h1Start">Find your inspiration.</h1>
+        <h2 className="splashPage__h2Start">Join the Flickr community, home to tens of billions of <br/>photos and 2 million groups.</h2>
+        <br/>
+        <br/>
+        <button className="splashPage__buttonStart">
+          Start for free
+        </button>
       </div>
     </div>
   );
--- client/src/images.ts
+++ client/src/images.ts
@@ -8,11 +8,11 @@ const images = [
         alt: 'Morotai Island'
     },
     {
-        src: 'https://images.unsplash.com/photo-1535930891776-0c2dfb7fda1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8ZG9nc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+        src: 'https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
         alt: 'Smart Dog'
     },
     {
-        src: 'https://images.unsplash.com/photo-1628082878598-ed6b930efb74?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG5hdHVyYWwlMjBiZWF1dHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+        src: 'https://images.unsplash.com/photo-1510797215324-95aa89f43c33?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
         alt: 'Gdynia, Polen'
     },
     {
@@ -20,7 +20,7 @@ const images = [
         alt: 'Torwood, Larbert, UK'
     },
     {
-        src: 'https://images.unsplash.com/photo-1560743641-3914f2c45636?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGRvZ3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+        src: 'https://images.unsplash.com/photo-1461301214746-1e109215d6d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
         alt: 'Happy Dogs'
     },
     {
